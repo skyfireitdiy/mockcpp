105db194ed2f6feac8d4b87e7abe893a91fff882
skyfire <skyfireitdiy@hotmail.com>
2021-10-22 07:24:03 +0800

    修改编译脚本

--- build_install4gtest.sh
+++ build_install4gtest.sh
@@ -2,18 +2,15 @@
 # build and install
 
 # there is a $xunit_home/include/gtest/gtest.h
-xunit_home=/usr/local
+xunit_home=$1
+install_dir=$2
 
-install_dir=/home/jelly/Programming/mockcpp-samples/mockcpp4gtest/mockcpp
+build_dir=build
 
-
-function build() {
-	mkdir $1 2>/dev/null
-	cd $1
-	cmake -DCMAKE_INSTALL_PREFIX=$install_dir -DMOCKCPP_XUNIT=gtest -DMOCKCPP_XUNIT_HOME=$xunit_home $2
-	make install
-}
-
-build ../../build_mockcpp_to_install4gtest ../mockcpp/mockcpp
-
-cd ../mockcpp/mockcpp
+mkdir -p $build_dir
+cd $build_dir
+cmake -DCMAKE_INSTALL_PREFIX=$install_dir -DMOCKCPP_XUNIT=gtest -DMOCKCPP_XUNIT_HOME=${xunit_home} ..
+make -j
+make install
+cd ..
+rm -rf $build_dir
